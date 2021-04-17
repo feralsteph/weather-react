6549133d4bbb6c2ddab7ca96a406533c858e464e
Stephanie LaRue <65689458+feralsteph@users.noreply.github.com>
2021-04-17 13:37:07 -0500

    added alt tag to img

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,10 @@ export default function Weather() {
       <h3>Houston</h3>
       <h4>Clear</h4>
       <h1>54°</h1>
-      <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"></img>
+      <img
+        src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+        alt="weather icon"
+      ></img>
     </div>
   );
 }
